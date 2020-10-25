c8260f7a45420b98b577bb16519ea2105dff571a
Roman Shevchuk <rshv00@gmail.com>
2020-10-25 09:59:14 +0200

    fix issues after moving project to root dir

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/portfolip-app'));
+app.use(express.static(__dirname + '/dist/portfolio-app'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/portfolip-app/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/portfolio-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
